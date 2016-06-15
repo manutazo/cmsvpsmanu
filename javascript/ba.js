9709bdd700833fea8090badb6f7fbbb33ca41ad3
manuel <manutazo4@hotmail.es>
2016-06-16 00:49:38 +0200

    sumome

---
+++ javascript/ba.js
@@ -0,0 +1,7 @@
+var
+_ba_cv="2.3.7",_baSrcPath="//plus.browsealoud.com/modules/",_baSrcFile="browsealoud.min.js",_baCsrcFile="ba.js",_baApplicationServer="plus.browsealoud.com",_baRes
+ourceServer="plus.browsealoud.com",_baPdfServer="pdf.browsealoud.com",_baPanelMode=!0,_ba_region=0,_ba_loader=function()
+{return{version:"",applicationPath:_baApplicationServer,assetPath:_baResourceServer,resourcesPath:location.protocol+"//"+_baApplicationServer,isSecure:"https:"===
+location.protocol,callback:null,settings:{isReady:!1,jsFilesList:
+[location.hostname],jsFileHit:"",validExpiry:!1,validFolder:!0,pageLanguage:0,chosenVoice:""},init:function(){var
+a=!1;null===document.getElementById("__ba_panel")&&document.write('<div id="__ba_panel"></div>'),this.getVersion(function(b){this.version=b;for(var 
